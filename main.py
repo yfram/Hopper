a50b9fbf6af8468c4346aab0280dc145a01b2fa5
Yoni <yonifram4@gmail.com>
2022-05-24 16:40:27 +0300

    Add motor logic

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import imp
 from picamera import PiCamera
 from time import sleep
 from lobe import ImageModel
+from gpiozero import Motor
+from enum import Enum
+
+class State(Enum):
+    APPLE = 0,
+    PEAR = 1
 
 camera = PiCamera()
-
 model = ImageModel.load('/home/pi/Downloads/Hopper/Tflite files/Apples')
 
-# Take Photo
-
+motor_state = State.APPLE
+motor = Motor(forward=4, backward=14)
+if motor.value == 1:
+    motor.stop()
 
 def take_photo():
     camera.start_preview()
@@ -16,15 +24,26 @@ def take_photo():
     camera.stop_preview()
     sleep(1)
 
-# Identify prediction and turn on appropriate LED
-
+def change_state(state):
+    global motor_state
+    if motor_state == state:
+        return
+    motor_state = state
+    if motor_state == State.APPLE:
+        motor.forward()
+        sleep(1)
+        motor.stop()
+    else:
+        motor.backward()
+        sleep(1)
+        motor.stop()
 
 def solve(label):
     print(label)
     if label == "Apples":
-        print("Apples")
+        change_state(State.APPLE)
     if label == "Pears":
-        print("Pears")
+        change_state(State.PEAR)
     else:
         raise Exception("No label found")
 
