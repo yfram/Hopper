4fed380feea29407351fefc0dcf5ea711c2d7f03
Yoni <yonifram4@gmail.com>
2022-05-24 15:07:19 +0300

    Add ML logic

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from picamera import PiCamera
 from time import sleep
+from lobe import ImageModel
 
 camera = PiCamera()
 
-camera.start_preview()
-sleep(5)
-camera.stop_preview()
+model = ImageModel.load('/Tflite files/Apples')
+
+# Take Photo
+def take_photo():
+    camera.start_preview()
+    sleep(2)
+    camera.capture('/home/pi/Pictures/image.jpg')
+    camera.stop_preview()
+    sleep(1)
+
+# Identify prediction and turn on appropriate LED
+def solve(label):
+    print(label)
+    if label == "Apples":
+        print("Apples")
+    if label == "Pears":
+        print("Pears")
+    else:
+        raise Exception("No label found")
+
+take_photo()
+# Run photo through Lobe TF model
+result = model.predict_from_file('/home/pi/Pictures/image.jpg')
+solve(result)
